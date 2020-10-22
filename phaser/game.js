9d46187254eacbed709bd4360ea0af7da1af8bbf
Dan Gutt <Dan Gutt>
2020-10-22 09:20:10 -0500

    Initial commit

---
+++ phaser/game.js
@@ -0,0 +1,40 @@
+var config = {
+  type: Phaser.AUTO,
+  width: 800,
+  height: 600,
+  physics: {
+      default: 'arcade',
+      arcade: {
+          gravity: { y: 300 },
+          debug: false
+      }
+  },
+  scene: {
+      preload: preload,
+      create: create,
+      update: update
+  }
+};
+
+var player;
+var stars;
+var bombs;
+var platforms;
+var cursors;
+var score = 0;
+var gameOver = false;
+var scoreText;
+
+var game = new Phaser.Game(config);
+
+function preload (){
+  // Load assets here
+}
+
+function create (){
+  // Set rules that don't change here
+}
+
+function update (){
+  // Set rules for things that change 
+}
