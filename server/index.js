d174c68f2e9048acbff7427b8a5a1367b5ba0069
Dan Gutt <dgutt10@gmail.com>
2020-10-22 20:35:34 -0500

    Added websockets

--- server/index.js
+++ server/index.js
@@ -5,12 +5,30 @@ const server = require('http').Server(app)
 const io = require('socket.io').listen(server)
 const port = 3000
 
+const players = {}
+
 app.use('/', express.static('phaser'));
 
 io.on('connection', function (socket) {
   console.log('a user connected');
+  players[socket.id] = {
+    rotation: 0,
+    x: Math.floor(Math.random() * 700) + 50,
+    y: Math.floor(Math.random() * 500) + 50,
+    playerId: socket.id,
+    team: (Math.floor(Math.random() * 2) == 0) ? 'red' : 'blue'
+  };
+  console.log('players: ', players)
+  // send the players object to the new player
+  socket.emit('currentPlayers', players);
+  // update all other players of the new player
+  socket.broadcast.emit('newPlayer', players[socket.id]);
   socket.on('disconnect', function () {
     console.log('user disconnected');
+    // remove this player from our players object
+    delete players[socket.id];
+    // emit a message to all players to remove this player
+    io.emit('disconnect', socket.id);
   });
 });
 
