1edd6a42dd3132721f556f797084751a68fa33d5
Angel Medina <angelmmed12@gmail.com>
2020-10-30 10:15:01 -0500

    basic ship movement

--- phaser/game.js
+++ phaser/game.js
@@ -1,11 +1,11 @@
 var config = {
   type: Phaser.AUTO,
-  width: 800,
-  height: 600,
+  width: 1000,
+  height: 800,
   physics: {
       default: 'arcade',
       arcade: {
-          gravity: { y: 300 },
+          gravity: { y: 0 },
           debug: false
       }
   },
@@ -16,26 +16,60 @@ var config = {
   }
 };
 
-var player;
-var stars;
-var bombs;
-var platforms;
-var cursors;
+var player1;
+var player2;
+var asteroids;
 var score = 0;
 var gameOver = false;
 var scoreText;
+var cursors;
 
 var game = new Phaser.Game(config);
 
 function preload (){
-  // Load assets here
+  this.load.spritesheet('asteroids', 'assets/asteroids.png', { frameWidth: 70, frameHeight: 65 })
+  this.load.spritesheet('ship', 'assets/ship.png', { frameWidth: 90, frameHeight: 90 })
 }
 
 function create (){
-  // Set rules that don't change here
-  this.socket = io()
+  this.socket = io();
+  asteroids = this.physics.add.group();
+  player1 = this.physics.add.sprite(400, 300, 'ship', 0);
+  cursors = this.input.keyboard.createCursorKeys();
+  for (let i = 0; i < 12; i++) {
+    const xPos = Phaser.Math.Between(0, 800);
+    const yPos = Phaser.Math.Between(0, 600);
+    const xVel = Phaser.Math.Between(-80, 80);
+    const yVel = Phaser.Math.Between(-80, 80);
+    let asteroid = asteroids.create(xPos, yPos, 'asteroids', 6).setScale(Phaser.Math.FloatBetween(0.5, 3))
+    asteroid.setVelocity(xVel, yVel);
+  }
 }
 
 function update (){
-  // Set rules for things that change 
+  if (cursors.up.isDown)
+  {
+    this.physics.velocityFromRotation(player1.rotation, 200, player1.body.acceleration);
+  }
+  else if (cursors.down.isDown)
+  {
+    this.physics.velocityFromRotation(player1.rotation, -200, player1.body.acceleration);
+  }
+  else
+  {
+    player1.setAcceleration(0);
+  }
+
+  if (cursors.left.isDown)
+  {
+    player1.setAngularVelocity(-300);
+  }
+  else if (cursors.right.isDown)
+  {
+    player1.setAngularVelocity(300);
+  }
+  else
+  {
+    player1.setAngularVelocity(0);
+  }
 }
