bea3e0a5aa181809634d833b571375642b849fd9
Dan Gutt <Dan Gutt>
2020-11-02 08:57:11 -0600

    Got player 1 up with websocket rendering.

--- phaser/game.js
+++ phaser/game.js
@@ -32,44 +32,56 @@ function preload (){
 }
 
 function create (){
+  var self = this
   this.socket = io();
-  asteroids = this.physics.add.group();
-  player1 = this.physics.add.sprite(400, 300, 'ship', 0);
+  this.socket.on('currentPlayers', function (players) {
+    Object.keys(players).forEach(function (id) {
+      if (players[id].playerId === self.socket.id) {
+        addPlayer(self, players[id]);
+      }
+    });
+  });
+  // player1 = this.physics.add.sprite(400, 300, 'ship', 0);
   cursors = this.input.keyboard.createCursorKeys();
-  for (let i = 0; i < 12; i++) {
-    const xPos = Phaser.Math.Between(0, 800);
-    const yPos = Phaser.Math.Between(0, 600);
-    const xVel = Phaser.Math.Between(-80, 80);
-    const yVel = Phaser.Math.Between(-80, 80);
-    let asteroid = asteroids.create(xPos, yPos, 'asteroids', 6).setScale(Phaser.Math.FloatBetween(0.5, 3))
-    asteroid.setVelocity(xVel, yVel);
-  }
+  // asteroids = this.physics.add.group();
+  // for (let i = 0; i < 12; i++) {
+  //   const xPos = Phaser.Math.Between(0, 800);
+  //   const yPos = Phaser.Math.Between(0, 600);
+  //   const xVel = Phaser.Math.Between(-80, 80);
+  //   const yVel = Phaser.Math.Between(-80, 80);
+  //   let asteroid = asteroids.create(xPos, yPos, 'asteroids', 6).setScale(Phaser.Math.FloatBetween(0.5, 3))
+  //   asteroid.setVelocity(xVel, yVel);
+  // }
 }
 
 function update (){
-  if (cursors.up.isDown)
-  {
-    this.physics.velocityFromRotation(player1.rotation, 200, player1.body.acceleration);
-  }
-  else if (cursors.down.isDown)
-  {
-    this.physics.velocityFromRotation(player1.rotation, -200, player1.body.acceleration);
-  }
-  else
-  {
-    player1.setAcceleration(0);
-  }
+  // if (cursors.up.isDown)
+  // {
+  //   this.physics.velocityFromRotation(player1.rotation, 200, player1.body.acceleration);
+  // }
+  // else if (cursors.down.isDown)
+  // {
+  //   this.physics.velocityFromRotation(player1.rotation, -200, player1.body.acceleration);
+  // }
+  // else
+  // {
+  //   player1.setAcceleration(0);
+  // }
 
-  if (cursors.left.isDown)
-  {
-    player1.setAngularVelocity(-300);
-  }
-  else if (cursors.right.isDown)
-  {
-    player1.setAngularVelocity(300);
-  }
-  else
-  {
-    player1.setAngularVelocity(0);
-  }
+  // if (cursors.left.isDown)
+  // {
+  //   player1.setAngularVelocity(-300);
+  // }
+  // else if (cursors.right.isDown)
+  // {
+  //   player1.setAngularVelocity(300);
+  // }
+  // else
+  // {
+  //   player1.setAngularVelocity(0);
+  // }
+}
+
+function addPlayer(self, playerInfo){
+  self.ship = self.physics.add.sprite(400, 300, 'ship', 0);
 }
