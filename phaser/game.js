56b6d8be9df661740fabd40c6a522b6d0cdd9909
Angel Medina <angelmmed12@gmail.com>
2020-11-13 11:45:49 -0600

    initial laser logic

--- phaser/game.js
+++ phaser/game.js
@@ -23,6 +23,7 @@ let score = 0;
 let gameOver = false;
 let scoreText;
 let cursors;
+let lastFired = 100;
 
 let game = new Phaser.Game(config);
 
@@ -68,9 +69,12 @@ function create (){
      })
   })
   this.cursors = this.input.keyboard.createCursorKeys();
+
+  // Lasers
+  this.laserGroup = new LaserGroup(this);
 }
 
-function update (){
+function update (time){
   if(this.ship){
     if (this.cursors.up.isDown)
     {
@@ -98,6 +102,11 @@ function update (){
       this.ship.setAngularVelocity(0);
     }
 
+    if (this.cursors.space.isDown && time > lastFired + 50) {
+      this.laserGroup.fireLaser(this.ship.x, this.ship.y, this.ship.rotation);
+      lastFired = time;
+    }
+
     let x = this.ship.x
     let y = this.ship.y
     let r = this.ship.rotation
@@ -122,4 +131,49 @@ function addPlayer(self, playerInfo){
 function addOtherPlayers(self, playerInfo){
   const otherPlayer = self.physics.add.sprite(playerInfo.x, playerInfo.y, 'ship', 0);
   self.otherPlayers[playerInfo.playerId] = otherPlayer
+}
+
+class LaserGroup extends Phaser.Physics.Arcade.Group
+{
+  constructor(scene) {
+    super(scene.physics.world, scene);
+
+    this.createMultiple({
+      classType: Laser,
+      frameQuantity: 30, // 30 instances of Laser
+      active: false,
+      visible: false,
+      key: 'laser'
+    })
+  }
+
+  fireLaser(x, y, r) {
+    const laser = this.getFirstDead(true);
+    if (laser) {
+      laser.fire(x, y, r);
+    }
+  }
+}
+
+class Laser extends Phaser.Physics.Arcade.Sprite {
+  constructor(scene, x, y) {
+    super(scene, x, y, 'laser');
+  }
+
+  fire(x, y, r) {
+    this.body.reset(x, y);
+    this.setActive(true);
+    this.setVisible(true);
+    this.setAngle(r)
+    this.scene.physics.velocityFromRotation(r, 400, this.body.velocity);
+  }
+
+  preUpdate(time, delta) {
+    super.preUpdate(time, delta);
+
+    if (this.y <= 0) {
+      this.setActive(false);
+      this.setVisible(false);
+    }
+  }
 }
