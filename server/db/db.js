4df80b393c491cad7cbf1acf10ed30105f836c63
Dan Gutt <dgutt10@gmail.com>
2021-01-29 16:29:42 -0600

    changed postgres path

--- server/db/db.js
+++ server/db/db.js
@@ -1,7 +1,7 @@
 const Sequelize = require('sequelize');
 const db = new Sequelize(
   process.env.DATABASE_URL ||
-  'postgres://localhost:5432/stackchat', {
+  'postgres://localhost:5432/space_dev', {
     logging: false
   }
 );
