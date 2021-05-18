4783c58346e087217ad169acfa846c1c2aebfc21
Dan Gutt <Dan Gutt>
2021-05-17 22:16:17 -0500

    Adding logging for debugging

--- models/index.js
+++ models/index.js
@@ -5,7 +5,9 @@ const path = require('path');
 const Sequelize = require('sequelize');
 const basename = path.basename(__filename);
 const env = process.env.NODE_ENV || 'development';
+console.log('env: ', env)
 const config = require(__dirname + '/../config/config.json')[env];
+console.log('config: ', config)
 const db = {};
 
 let sequelize;
