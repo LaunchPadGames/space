565df516863c50c622bcee4ed1dcf3d24e394ec9
Dan Gutt <Dan Gutt>
2021-05-17 22:38:44 -0500

    Connecting to redis on heroku

--- server/redis/index.js
+++ server/redis/index.js
@@ -1,7 +1,7 @@
 const redis = require('redis');
 const bluebird = require('bluebird')
 bluebird.promisifyAll(redis)
-const redisClient = redis.createClient();
+const redisClient = redis.createClient(process.env.REDIS_URL);
 
 redisClient.on("error", function(error) {
   console.error(error);
