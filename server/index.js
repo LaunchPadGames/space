21e772b578421d431ee86a95726c6966a7a3cd7b
Dan Gutt <Dan Gutt>
2021-05-17 22:03:05 -0500

    Prepping port for heroku usage

--- server/index.js
+++ server/index.js
@@ -4,7 +4,7 @@ const app = express()
 // const db = require('./db');
 const server = require('http').Server(app)
 const io = require('socket.io').listen(server)
-const port = 3000
+const port = process.env.PORT || 3000
 
 app.use('/', express.static('phaser'));
 
