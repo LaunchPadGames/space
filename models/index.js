edd1c34ff669e1b729290f5da252b971e8f78f9f
Dan Gutt <Dan Gutt>
2021-05-17 22:18:16 -0500

    Bug fix

--- models/index.js
+++ models/index.js
@@ -11,7 +11,7 @@ console.log('config: ', config)
 const db = {};
 
 let sequelize;
-if (config.use_env_variable) {
+if (config && config.use_env_variable) {
   sequelize = new Sequelize(process.env[config.use_env_variable], config);
 } else {
   sequelize = new Sequelize(config.database, config.username, config.password, config);
