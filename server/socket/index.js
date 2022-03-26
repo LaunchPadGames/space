e8fdc45e96a8d76a33abffa709d58fd86cdbeb3c
Dan Gutt <dgutt10@gmail.com>
2022-03-26 15:03:43 -0500

    Removed redis completely and fixed the destroy powerup sync and the power up use sync.

--- server/socket/index.js
+++ server/socket/index.js
@@ -20,9 +20,9 @@ module.exports = io => {
       defaults: { roomTag: roomTag, playerLimit: allowedPlayersCount}
     });
     let game = games[0]
-    if(!(await redisGetter(roomTag)) ){
-      let game_cache = new GameCache()
-      redisSetter(roomTag, game_cache)
+    if(!global.global.game_cache){
+      global.game_cache = new GameCache()
+      // redisSetter(roomTag, global.game_cache)
     } 
 
     await Player.create({socketId: socket.id, gameId: game.dataValues.id})
@@ -32,7 +32,7 @@ module.exports = io => {
       where: { gameId: game.dataValues.id }
     })
 
-    let game_cache =  await redisGetter(roomTag)
+    // let game_cache =  await redisGetter(roomTag)
     const playerLimit = game.dataValues.playerLimit
     if (currentPlayersCount > playerLimit) {
       socket.emit('inProgress');
@@ -41,38 +41,38 @@ module.exports = io => {
       const room = currentRoom(io, socket)
       // let redisGame = await redisGetter(room)
       // redisGame['players'][socket.id] = createPlayer(socket, currentPlayersCount)
-      game_cache['players'][socket.id] = createPlayer(socket, currentPlayersCount)
+      global.game_cache['players'][socket.id] = createPlayer(socket, currentPlayersCount)
       // console.log('player: ', redisGame['players'])
       // await redisSetter(room, redisGame)
       
       // send the players object to the new player
-      socket.emit('currentPlayers', game_cache['players']);
+      socket.emit('currentPlayers', global.game_cache['players']);
 
       // update all other players of the new player
-      socket.to(room).broadcast.emit('newPlayer', game_cache['players'][socket.id]);
+      socket.to(room).broadcast.emit('newPlayer', global.game_cache['players'][socket.id]);
       if (currentPlayersCount !== playerLimit) {
-        redisSetter(roomTag, game_cache)
+        redisSetter(roomTag, global.game_cache)
         let base_url = process.env.BASE_URL || 'http://localhost:3000'
         
-        socket.emit('waitingForPlayers', { roomTag: roomTag, time: game_cache['time'], baseUrl: base_url });
+        socket.emit('waitingForPlayers', { roomTag: roomTag, time: global.game_cache['time'], baseUrl: base_url });
       } else {
         const asteroidData = createAsteroids()
         // redisGame = await redisGetter(room)
-        game_cache['asteroids'] = asteroidData['asteroidHash']
-        let time = game_cache['time']
+        global.game_cache['asteroids'] = asteroidData['asteroidHash']
+        let time = global.game_cache['time']
         let intervalId = setInterval(async function(){
-          if(time === 0) clearInterval(game_cache['intervalId'])
+          if(time === 0) clearInterval(global.game_cache['intervalId'])
           time = time - 1
           io.sockets.in(room).emit('updateTimer', time);
         }, 1000)
-        game_cache['intervalId'] = intervalId[Symbol.toPrimitive]()
+        global.game_cache['intervalId'] = intervalId[Symbol.toPrimitive]()
         io.sockets.in(room).emit('createAsteroids', asteroidData['asteroidArray'])
       }
       socket.on('disconnect', async function () {
         console.log('user disconnected');
         // remove this player from our players object
         // redisGame = await redisGetter(room)
-        delete game_cache['players'][socket.id]
+        delete global.game_cache['players'][socket.id]
         // redisSetter(room, redisGame)
         // delete players[room][socket.id];
         // emit a message to all players to remove this player
@@ -80,16 +80,16 @@ module.exports = io => {
       });
       socket.on('playerMovement', async function(movementData){
         // redisGame = await redisGetter(room)
-        game_cache['players'][socket.id].x = movementData.x
-        game_cache['players'][socket.id].y = movementData.y
-        game_cache['players'][socket.id].rotation = movementData.rotation
+        global.game_cache['players'][socket.id].x = movementData.x
+        global.game_cache['players'][socket.id].y = movementData.y
+        global.game_cache['players'][socket.id].rotation = movementData.rotation
         // redisSetter(room, redisGame)
-        socket.to(room).broadcast.emit('playerMoved', game_cache['players'][socket.id])
+        socket.to(room).broadcast.emit('playerMoved', global.game_cache['players'][socket.id])
       })
 
       socket.on('laserShot', async function(data) {
         // redisGame = await redisGetter(room)
-        if (game_cache['players'][socket.id] == null) return;
+        if (global.game_cache['players'][socket.id] == null) return;
         let laser = data;
         data.owner_id = socket.id;
         socket.to(room).broadcast.emit('laserUpdate', laser, socket.id)
@@ -98,24 +98,24 @@ module.exports = io => {
         let laser = data['laser']
         let asteroidIndex = data['asteroidIndex']
         // redisGame = await redisGetter(room)
-        if(laser && game_cache['asteroids'][asteroidIndex]){
-          game_cache['players'][socket.id]['score'] += 10
+        if(laser && global.game_cache['asteroids'][asteroidIndex]){
+          global.game_cache['players'][socket.id]['score'] += 10
         }
-        game_cache['asteroids'][asteroidIndex] = false
+        global.game_cache['asteroids'][asteroidIndex] = false
         // redisSetter(room, redisGame)
         socket.to(room).broadcast.emit('broadcastDestoryAsteroid', asteroidIndex)
-        io.sockets.in(room).emit('updateScore', {socketId: socket.id, score: game_cache['players'][socket.id]['score']})
+        io.sockets.in(room).emit('updateScore', {socketId: socket.id, score: global.game_cache['players'][socket.id]['score']})
 
         if(laser){
           let powerupNum = Math.floor(Math.random() * 100)
            if(powerupNum > 50 && powerupNum < 76){
             let powerupId = tagGenerator()
-            game_cache['powerups'][powerupId] = true
+            global.game_cache['powerups'][powerupId] = true
             // redisSetter(room, redisGame)
             io.sockets.in(room).emit('updatePowerups', {id: powerupId, x: data['x'], y: data['y'], type: 'gold_powerup'})
           } if (powerupNum > 76) {
             let powerupId = tagGenerator()
-            game_cache['powerups'][powerupId] = true
+            global.game_cache['powerups'][powerupId] = true
             // redisSetter(room, redisGame)
             io.sockets.in(room).emit('updatePowerups', {id: powerupId, x: data['x'], y: data['y'], type: 'shield_powerup'})
           }
@@ -135,10 +135,10 @@ module.exports = io => {
         // redisGame = await redisGetter(room)
         console.log('power up: ', powerupId)
         console.log('type: ', type)
-        console.log("game_cache: ", game_cache)
-        console.log("game_cache['powerups'][powerupId]: ", game_cache['powerups'][powerupId])
-        if(game_cache['powerups'][powerupId]){
-          game_cache['powerups'][powerupId] = false
+        console.log("global.game_cache: ", global.game_cache)
+        console.log("global.game_cache['powerups'][powerupId]: ", global.game_cache['powerups'][powerupId])
+        if(global.game_cache['powerups'][powerupId]){
+          global.game_cache['powerups'][powerupId] = false
           // redisSetter(room, redisGame)
           if(type === 'shield_powerup'){
             io.sockets.in(room).emit('shieldPowerUp', {powerupId: powerupId, texture: 'ship_shield1', level: 2, playerId: socket.id})
@@ -147,7 +147,7 @@ module.exports = io => {
             io.sockets.in(room).emit('silverPowerup', {powerupId: powerupId, playerId: socket.id})
             let timeoutObject = setTimeout(async function() {
               // let redisGame = await redisGetter(room)
-              let timeoutId = game_cache['players'][socket.id]['powerups']['rateOfFire'] 
+              let timeoutId = global.game_cache['players'][socket.id]['powerups']['rateOfFire'] 
               if(timeoutId === this[Symbol.toPrimitive]()){
                 io.sockets.in(room).emit('silverPowerupOff', {playerId: socket.id})
               }
@@ -155,11 +155,11 @@ module.exports = io => {
             // if(!redisGame['players'][socket.id]['powerups']){
             //   redisGame['players'][socket.id]['powerups'] = {}
             // }
-            game_cache['players'][socket.id]['powerups']['rateOfFire'] = timeoutObject[Symbol.toPrimitive]()
+            global.game_cache['players'][socket.id]['powerups']['rateOfFire'] = timeoutObject[Symbol.toPrimitive]()
             // redisSetter(room, redisGame)
           } 
           if(type === 'gold_powerup'){
-            let spray_queue = game_cache['players'][socket.id]['powerups']['spray_queue']
+            let spray_queue = global.game_cache['players'][socket.id]['powerups']['spray_queue']
             spray_queue.enqueue(powerupId)
             io.sockets.in(room).emit('goldPowerup', {powerupId: powerupId, playerId: socket.id})
               if(spray_queue.size === 1){
@@ -176,7 +176,7 @@ module.exports = io => {
             io.sockets.in(room).emit('starPowerup', {powerupId: powerupId, playerId: socket.id})
             let timeoutObject = setTimeout(async function() {
               // let redisGame = await redisGetter(room)
-              let timeoutId = game_cache['players'][socket.id]['powerups']['speed'] 
+              let timeoutId = global.game_cache['players'][socket.id]['powerups']['speed'] 
               if(timeoutId === this[Symbol.toPrimitive]()){
                 io.sockets.in(room).emit('starPowerupOff', {playerId: socket.id})
               }
@@ -184,7 +184,7 @@ module.exports = io => {
             // if(!redisGame['players'][socket.id]['powerups']){
             //   redisGame['players'][socket.id]['powerups'] = {}
             // } 
-            game_cache['players'][socket.id]['powerups']['speed'] = timeoutObject[Symbol.toPrimitive]()
+            global.game_cache['players'][socket.id]['powerups']['speed'] = timeoutObject[Symbol.toPrimitive]()
             // redisSetter(room, redisGame)
           }
         }
